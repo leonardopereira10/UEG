ea9df1fcfb5409bd96a820570cbbc9ed484146fe
Lucas Moraes <lucasdemoraesc@gmail.com>
2019-10-18 09:52:33 -0300

    Classe cpf

---
+++ SistemaDeMatriculas-V0/cpf.h
@@ -0,0 +1,23 @@
+#ifndef CPF_H
+#define CPF_H
+
+#include <QMainWindow>
+
+class Cpf
+{
+public:
+	void setCpf(const QString &value);
+
+	bool validCpf();
+
+protected:
+	int* cpfToInt();
+	bool knownValid(const int *);
+	bool validDigitOne(const int *);
+	bool validDigitTwo(const int *);
+
+private:
+	QString cpf;
+};
+
+#endif // CPF_H
