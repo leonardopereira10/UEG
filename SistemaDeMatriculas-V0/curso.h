d06f3c00ebaa84ca4a3e14ae133d68655b1e7780
leonardopereira10 <leonardopereira10@gmail.com>
2019-10-19 01:50:00 -0300

    criação de lista de disciplinas

--- SistemaDeMatriculas-V0/curso.h
+++ SistemaDeMatriculas-V0/curso.h
@@ -1,9 +1,14 @@
 #ifndef CURSO_H
 #define CURSO_H
 
+#include <initializer_list>
+#include "disciplinas.h"
+
+using namespace std;
 
 class Curso
 {
+    initializer_list<Disciplinas> matrizCurricular;
 public:
     Curso();
 };
--- SistemaDeMatriculas-V0/disciplinas.h
+++ SistemaDeMatriculas-V0/disciplinas.h
@@ -1,9 +1,16 @@
 #ifndef DISCIPLINAS_H
 #define DISCIPLINAS_H
 
+#include <QString>
+
+
 
 class Disciplinas
 {
+private:
+    QString codDisciplina;
+    QString nomeDisciplina;
+    QString cargaHoraria;
 public:
     Disciplinas();
 };
