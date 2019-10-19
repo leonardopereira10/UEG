57fbac8204ebedf21b83cfb58a29e620d409749c
leonardopereira10 <leonardopereira10@gmail.com>
2019-10-18 23:28:50 -0300

    definição especifica e restritiva do construtor para não ser inicializado uma 'pessoa' vazia;

--- SistemaDeMatriculas-V0/pessoas.cpp
+++ SistemaDeMatriculas-V0/pessoas.cpp
@@ -1,6 +1,22 @@
 #include "pessoas.h"
 
-Pessoas::Pessoas()
+Pessoas::Pessoas(
+                    QString CPF,
+                    QString nome,
+                    QString endereco,
+                    QString setor,
+                    QString cidade,
+                    QString estado,
+                    QString celular,
+                    QString email
+                )
 {
-
+    this->CPF = CPF;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->setor = setor;
+    this->cidade = cidade;
+    this->estado = estado;
+    this->celular = celular;
+    this->email = email;
 }
--- SistemaDeMatriculas-V0/pessoas.h
+++ SistemaDeMatriculas-V0/pessoas.h
@@ -7,7 +7,16 @@ class Pessoas
 {
 protected:
 	QString CPF, nome, endereco, setor, cidade, estado, celular, email;
-    Pessoas();
+    Pessoas(
+            QString CPF,
+            QString nome,
+            QString endereco,
+            QString setor,
+            QString cidade,
+            QString estado,
+            QString celular,
+            QString email
+            );
 };
 
 #endif // PESSOAS_H
