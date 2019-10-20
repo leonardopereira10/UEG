78b86a3ee338f866d358b15f0d62b4d4e8863664
Lucas Moraes <lucasdemoraesc@gmail.com>
2019-10-19 21:28:41 -0300

    Iniciando módulo aluno

---
+++ SistemaDeMatriculas-V0/aluno.cpp
@@ -0,0 +1,12 @@
+#include "aluno.h"
+
+Aluno::Aluno()
+{
+
+}
+
+Aluno::Aluno(int matricula, int ano)
+{
+	this->matricula = matricula;
+	this->ano = ano;
+}
---
+++ SistemaDeMatriculas-V0/aluno.h
@@ -0,0 +1,18 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#include "pessoa.h"
+
+class Aluno : protected Pessoa
+{
+public:
+	Aluno();
+	Aluno(int matricula, int ano);
+
+
+private:
+	int matricula;
+	int ano;
+};
+
+#endif // ALUNO_H
