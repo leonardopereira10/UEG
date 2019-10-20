5cd834eb7c68bcd0dc90a8f3231327be1af2cc99
danyelliasmanso <danyellias@hotmail.com>
2019-10-20 14:42:13 -0300

    adicionar ui

--- SistemaDeMatriculas-V0/cpf.cpp
+++ SistemaDeMatriculas-V0/cpf.cpp
@@ -1,8 +1,30 @@
-﻿#include "cpf.h"
+#include "cpf.h"
+#include "ui_cpf.h"
+
+Cpf::Cpf(QWidget *parent) :
+
+	QDialog(parent),
+	ui(new Ui::Cpf)
+{
+    ui->setupUi(this);
+    ui->campo_cpf->setFocus();
+    ui->btn_ok->setFocus();
+
+}
+
+Cpf::~Cpf()
+{
+	delete ui;
+}
+
+QString Cpf::getCpf() const
+{
+	return cpf;
+}
 
 void Cpf::setCpf(const QString &value)
 {
-  cpf = value;
+	cpf = value;
 }
 
 int *Cpf::cpfToInt()
@@ -16,8 +38,8 @@ int *Cpf::cpfToInt()
   // Converter para int
   for (int i = 0; i < 11; i++)
   {
-    tmpCpf[i] = cpf.at(i);
-    vetCpf[i] = tmpCpf[i].toInt();
+	tmpCpf[i] = cpf.at(i);
+	vetCpf[i] = tmpCpf[i].toInt();
   }
   return vetCpf;
 }
@@ -27,10 +49,10 @@ bool Cpf::knownValid(const int *vetCpf)
   // Cpfs inválidos conhecidos (222.222.222-22)
   for (int i = 0, f = 0; i < 11; i++)
   {
-    if (vetCpf[i] == vetCpf[i + 1])
-      f++;
-    if (f == 10)
-      return false;
+	if (vetCpf[i] == vetCpf[i + 1])
+	  f++;
+	if (f == 10)
+	  return false;
   }
   return true;
 }
@@ -43,15 +65,15 @@ bool Cpf::validDigitOne(const int *vetCpf)
   // Soma do primeiro dígito
   for (int i = 0, f = 10; i < 9; i++)
   {
-    sumDigitOne += (vetCpf[i] * f);
-    f--;
+	sumDigitOne += (vetCpf[i] * f);
+	f--;
   }
   restDigitOne = ((sumDigitOne * 10) % 11);
   if (restDigitOne == 10)
-    restDigitOne = 0; // Se for igual a 10 considera 0
+	restDigitOne = 0; // Se for igual a 10 considera 0
 
   if (restDigitOne == vetCpf[9])
-    return true;
+	return true;
   return false;
 }
 
@@ -62,29 +84,61 @@ bool Cpf::validDigitTwo(const int *vetCpf)
   int restDigitTwo = 0;
   for (int i = 0, f = 11; i < 10; i++)
   {
-    sumDigitTwo += (vetCpf[i] * f);
-    f--;
+	sumDigitTwo += (vetCpf[i] * f);
+	f--;
   }
   restDigitTwo = ((sumDigitTwo * 10) % 11);
   if (restDigitTwo == 10)
-    restDigitTwo = 0; // Se for igual a 10 considera 0
+	restDigitTwo = 0; // Se for igual a 10 considera 0
 
   if (restDigitTwo == vetCpf[10])
-    return true;
+	return true;
   return false;
 }
 
 bool Cpf::validCpf()
 {
-  if (cpf.length() < 14) // Considera-se a pontuação
-    return false;
+  if (cpf.length() < 14) // Considera-se a pontuaçaoo
+	return false;
   else
   {
-    int *vetCpf = cpfToInt();
+	int *vetCpf = cpfToInt();
 
-    if (knownValid(vetCpf))
-      if (validDigitOne(vetCpf) && validDigitTwo(vetCpf))
-        return true;
-    return false;
+	if (knownValid(vetCpf))
+	  if (validDigitOne(vetCpf) && validDigitTwo(vetCpf))
+		return true;
+	return false;
   }
 }
+
+
+// BUG: Janela fecha ao pressionar enter
+// TODO: IMAGEM do erro retirada
+void Cpf::on_btn_ok_clicked()
+{
+    QPixmap erro (":/imagens/Erro.png");
+    QPixmap valido (":/imagens/Confirmacao.png");
+    setCpf(ui->campo_cpf->text());
+    if(!validCpf()){
+        ui->validador->setPixmap(erro);
+        ui->campo_cpf->setFocus();
+        ui->campo_cpf->selectAll();
+    }
+    else{
+        ui->validador->setPixmap(valido);
+    }
+}
+
+
+void Cpf::on_btn_cancelar_clicked()
+{
+    this->close();
+}
+
+void Cpf::on_campo_cpf_returnPressed()
+{
+	ui->btn_ok->click();
+}
+
+// NOTE: Removido método de "Cancelar" (fechar janela)
+// Função de fechar adicionada ao botão por meio de signais
