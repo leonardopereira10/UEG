45e171b532c97de4edf9bdaa7764e226976aa441
danyelliasmanso <danyellias@hotmail.com>
2019-10-22 20:10:16 -0300

    1

---
+++ SistemaDeMatriculas-V0/professor.cpp
@@ -0,0 +1,6 @@
+#include "professor.h"
+
+Professor::Professor()
+{
+
+}
---
+++ SistemaDeMatriculas-V0/professor.h
@@ -0,0 +1,11 @@
+#ifndef PROFESSOR_H
+#define PROFESSOR_H
+
+
+class Professor
+{
+public:
+    Professor();
+};
+
+#endif // PROFESSOR_H
